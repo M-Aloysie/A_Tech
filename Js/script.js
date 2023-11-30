ee288e1ae6579bc09fe567e6c99b7808a763ff68
M-Aloysie <a.murekatet@alustudent.com>
2023-11-30 14:03:19 +0200

    New Commit

--- Js/script.js
+++ Js/script.js
@@ -1,54 +1,110 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.needs-validation');
+    const createAccountBtn = document.querySelector('.btn-warning');
+    const passwordInput = document.getElementById('inputAddress');
+    const confirmPasswordInput = document.getElementById('inputAddress2');
+    const passwordMismatchMessage = document.getElementById('passwordMismatchMessage');
+  
+    form.addEventListener('submit', function (event) {
+      let isValid = true;
+  
+      if (!form.checkValidity() || !checkPasswordsMatch()) {
+        event.preventDefault();
+        event.stopPropagation();
+        isValid = false;
+        displayPasswordMismatchMessage();
+      }
+  
+      // Check each form element and mark as invalid if needed
+      form.querySelectorAll(':invalid').forEach(function (element) {
+        element.classList.add('is-invalid');
+      });
+  
+      // Check if the form is valid and passwords match before redirecting
+      if (isValid && checkPasswordsMatch()) {
+        // Allow redirection to Signin.html if the form is valid
+        window.location.href = 'Signin.html';
+      }
+    });
+  
+    form.addEventListener('change', function (event) {
+      const target = event.target;
+  
+      if (target.hasAttribute('required')) {
+        if (!target.checkValidity()) {
+          target.classList.add('is-invalid');
         } else {
-            navbarCollapsible.classList.add('navbar-shrink')
+          target.classList.remove('is-invalid');
         }
+      }
+    });
+  
+    createAccountBtn.addEventListener('click', function (event) {
+      let isValid = true;
+  
+      // Check each form element and mark as invalid if needed
+      form.querySelectorAll(':invalid').forEach(function (element) {
+        element.classList.add('is-invalid');
+        isValid = false;
+      });
+  
+      // Check if the form is valid and passwords match before allowing redirection
+      if (!isValid || !checkPasswordsMatch()) {
+        event.preventDefault();
+        event.stopPropagation();
+        displayPasswordMismatchMessage();
+      }
+    });
+  
+    function checkPasswordsMatch() {
+      return passwordInput.value === confirmPasswordInput.value;
+    }
+  
+    function displayPasswordMismatchMessage() {
+      passwordMismatchMessage.style.display = 'block';
+    }
+  
+    // Event listener to hide the message when passwords match
+    passwordInput.addEventListener('input', function () {
+      hidePasswordMismatchMessage();
+    });
+  
+    confirmPasswordInput.addEventListener('input', function () {
+      hidePasswordMismatchMessage();
+    });
+  
+    function hidePasswordMismatchMessage() {
+      passwordMismatchMessage.style.display = 'none';
+    }
+  });
 
-    };
+  // script.js
 
-    // Shrink the navbar 
-    navbarShrink();
+function validateSignInForm() {
+    // Get input values
+    var email = document.getElementById('inputEmail3').value;
+    var password = document.getElementById('inputPassword3').value;
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+    // Assume you have a list of registered users
+    var registeredUsers = [
+        { email: 'user1@example.com', password: 'password1' },
+        { email: 'user2@example.com', password: 'password2' },
+        // Add more users as needed
+    ];
 
-    //  Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+    // Check if the entered email and password match any registered user
+    var isValidUser = registeredUsers.some(function (user) {
+        return user.email === email && user.password === password;
     });
 
-});
+    if (isValidUser) {
+        // Redirect or perform the sign-in logic
+        window.location.href = 'Gstarted.html';
+    } else {
+        // Display error message
+        var errorMessage = document.getElementById('signInError');
+        errorMessage.style.display = 'block';
+    }
+}
+
+  
